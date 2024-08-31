c15a4ea167ec40b4fc6a2951cdcaa9c3fd90a375
Divyanshu Parihar <divyanshu1447@gmail.com>
2024-08-31 21:45:19 +0530

    chore: Update TypeScript configuration and package dependencies + Transaction signer ```

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,59 @@ import {
   useWeb3Modal,
   useWeb3ModalAccount,
 } from "@web3modal/ethers/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ConnectButton from "@/components/ConnectWallet";
 import { SessionProvider } from "next-auth/react";
 import Nav from "@/components/Nav";
 import HomePage from "@/components/Home";
-
+import Web3 from "web3";
+// import { ContractAbi } from "web3-core";
+import MyContractABI from "@/artifacts/contracts/Escrow.sol/Escrow.json";
 export default function Home() {
   const { address, isConnected, status } = useWeb3ModalAccount();
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState<any>();
+
+  useEffect(() => {
+    console.log("here");
+    if (typeof window.ethereum !== "undefined") {
+      const web3 = new Web3(window.ethereum);
+
+      (window.ethereum as any)
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts) => {
+          setAccount(accounts[0]);
+
+          console.log("accounts", accounts);
+          const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+          const myContract = new web3.eth.Contract(
+            MyContractABI["abi"],
+            contractAddress
+          );
+
+          console.log("contract", myContract);
+          setContract(myContract);
+        })
+        .catch((error) => {
+          console.error("User denied account access", error);
+        });
+
+      (window.ethereum as any).on("accountsChanged", function (accounts) {
+        setAccount(accounts[0]);
+      });
+    } else {
+      console.error("MetaMask is not installed");
+    }
+  }, []);
+
+  const fetchGreeting = async () => {
+    if (contract) {
+      const greeting = await contract.methods.saySomething().call();
+      console.log(greeting);
+    } else {
+      console.log("No contract found");
+    }
+  };
   useEffect(() => {
     console.log({ address, isConnected, status });
   }, [address, isConnected, status]);
@@ -30,6 +75,7 @@ export default function Home() {
         </div>
 
         <div className="content absolute z-100 h-full w-full">
+          {/* <button onClick={fetchGreeting}>Get Greeting</button> */}
           <Nav />
           <HomePage />
         </div>
--- forms/newTransaction.tsx
+++ forms/newTransaction.tsx
@@ -14,6 +14,11 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { ethers, parseEther } from "ethers";
+import EscrowABI from "../artifacts/contracts/Escrow.sol/Escrow.json";
+import Web3 from "web3";
+import { useState } from "react";
+///
 const formSchema = z.object({
   receiverAddr: z.string().min(2, {
     message: "Please enter a valid address.",
@@ -23,7 +28,9 @@ const formSchema = z.object({
 });
 
 export function NewEscrowForm() {
-  // 1. Define your form.
+  const [provider, setProvider] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [value, setValue] = useState(null);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -32,10 +39,78 @@ export function NewEscrowForm() {
       description: "",
     },
   });
+  const makeContract = async () => {
+    // Connect to the Ethereum provider (e.g., MetaMask)
+
+    if (!window.ethereum) {
+      console.log("No metamask found");
+      return;
+    }
+
+    const provider = new ethers.BrowserProvider(window.ethereum as any);
+
+    await provider.send("eth_requestAccounts", []);
+    const signer = await provider.getSigner();
+    // setProvider(provider);
+
+    console.log("Signer address:", await signer.getAddress());
+    const contract = new ethers.Contract(
+      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      EscrowABI.abi,
+      signer
+    );
+
+    // const result = await contract.saySomething();
+    // con
+    const amount = parseEther(form.getValues("amount"));
+    // const amount = ethers.utils.parseEther(form.getValues("amount"));
+    await contract.createTask(
+      form.getValues("receiverAddr"),
+      amount,
+      form.getValues("description")
+    );
+    console.log("Done");
+    // console.log(result);
+    // // Create a contract instance
+    // const contract = new ethers.Contract(
+    //   contractAddress,
+    //   contractABI,
+    //   provider
+    // );
+    // setContract(contract);
+  };
+
+  // 1. Define your form.
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // const web3 = new Web3(ethereum);
+        await makeContract();
+        // const TaskContract = new web3.eth.Contract(
+        //   EscrowABI.abi,
+        //   "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+        // );
+        // let address = await TaskContract.methods.saySomething().call();
+        // console.log(address);
+        // // allTasks = allTasks.map((task) => ({
+        // //   id: task.id.toString(),
+        // //   taskText: task.taskText,
+        // //   wallet: task.wallet,
+        // //   taskDate: new Date(task.taskDate * 1000).toLocaleDateString(),
+        // //   taskTime: new Date(task.taskDate * 1000).toLocaleTimeString(),
+        // //   isDeleted: task.isDeleted,
+        // // }));
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Form {...form}>
